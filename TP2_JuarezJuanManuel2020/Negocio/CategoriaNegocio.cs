3518d238fdd7fbefe2f7ef6107174bb99500460f
juancinjuarez <juarezjuanmanuel73@gmail.com>
2020-04-27 19:11:42 -0300

    Add files via upload

---
+++ TP2_JuarezJuanManuel2020/Negocio/CategoriaNegocio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dominio;
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<CategoriaDominio> listar()
+        {
+            List<CategoriaDominio> listado = new List<CategoriaDominio>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "data source = LAPTOP-41LKJ1NF\\SQLEXPRESS; initial catalog = CATALOGO_DB;integrated security = sspi";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select Id,Descripcion from CATEGORIAS";
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+                while(lector.Read())
+                {
+                    CategoriaDominio aux = new CategoriaDominio();
+                    aux.Id = lector.GetInt32(0);
+                    aux.Descripcion = lector.GetString(1);
+                    listado.Add(aux);
+                }
+
+                return listado;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
---
+++ TP2_JuarezJuanManuel2020/Negocio/MarcaNegocio.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dominio;
+
+namespace Negocio
+{
+    public class MarcaNegocio
+    {
+        public List<MarcaDominio> listar()
+        {
+            List<MarcaDominio> listado = new List<MarcaDominio>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "data source = LAPTOP-41LKJ1NF\\SQLEXPRESS; initial catalog = CATALOGO_DB;integrated security = sspi";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select Id,Descripcion from MARCAS";
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+                while (lector.Read())
+                {
+                    MarcaDominio aux = new MarcaDominio();
+                    aux.Id = lector.GetInt32(0);
+                    aux.Descripcion = lector.GetString(1);
+                    listado.Add(aux);
+                }
+
+                return listado;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
